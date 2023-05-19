3e92d4c8d5b8a6ac8194a2b414b79ce221848587
serinryu <serin0404@naver.com>
2023-05-19 10:22:13 +0900

    Feat : Delete comment (#9)

--- src/controllers/commentController.js
+++ src/controllers/commentController.js
@@ -4,15 +4,31 @@ const Comment = db.Comment;
 
 export const postComment = async (req, res, next) => {
     try {
-        const { content } = req.body;
-        const { postid } = req.params;
+        const { content, PostId } = req.body;
         await Comment.create({
             content,
-            PostId: postid,
+            PostId,
             UserId: req.user.id,
         });
-        return res.redirect(`/posts/${postid}`);
+        return res.redirect(`/posts/${PostId}`);
     } catch (err) {
         console.error(err);
     }
-}
+};
+
+export const deleteComment = async (req, res, next) => {
+    const { id } = req.params; //commentid
+    try {
+        const comment = await Comment.findOne({
+            where: { id },
+        });
+        //session check
+        if(comment.UserId !== req.user.id){
+            return res.redirect(`/posts/${comment.PostId}`);
+        };
+        await comment.destroy();
+        return res.redirect(`/posts/${comment.PostId}`);
+    } catch (err) {
+        console.error(err);
+    }
+};
--- src/routes/commentRouter.js
+++ src/routes/commentRouter.js
@@ -1,16 +1,19 @@
 import express from "express";
 import { isLoggedIn } from "../middlewares/index.js";
-import { postComment } from "../controllers/commentController.js";
+import { postComment, deleteComment } from "../controllers/commentController.js";
 
 const commentRouter = express.Router();
 
 // /comments
-commentRouter.route('/:postid')
+commentRouter.route('/')
     .all(isLoggedIn)
     .post(postComment);
-commentRouter.route('/:postid/:commentid')
-    .put((req,res) => res.send('Edit comment'))
-    .patch((req,res) => res.send('Delete comment'));
-commentRouter.post('/:postid/:commentid/like', (req,res) => res.send('Like comment'));
+
+commentRouter.route('/:id')
+    .all(isLoggedIn)
+    //.put(editComment)
+    .patch(deleteComment); // /comments/:commentid
+
+commentRouter.post('/comment/:id/like', (req,res) => res.send('Like comment'));
 
 export default commentRouter;
